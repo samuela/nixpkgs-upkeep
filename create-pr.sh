9116ba822fa62e67192280f24e0166c3d0d9b032
Samuel Ainsworth <skainsworth@gmail.com>
2020-09-19 13:11:54 -0700

    typo

--- create-pr.sh
+++ create-pr.sh
@@ -4,7 +4,7 @@
 set -eou pipefail
 
 echo $PACKAGE
-echo $CURRENT_VERISON
+echo $CURRENT_VERSION
 
 # Check that there's a diff from the updater script. See https://stackoverflow.com/questions/3878624/how-do-i-programmatically-determine-if-there-are-uncommitted-changes.
 if git diff-index --quiet HEAD --; then
