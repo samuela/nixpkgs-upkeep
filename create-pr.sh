10b285812135dcb9153f74ef440d3e5f6f8fa821
Samuel Ainsworth <skainsworth@gmail.com>
2020-09-19 13:10:00 -0700

    wat?

--- create-pr.sh
+++ create-pr.sh
@@ -3,6 +3,9 @@
 
 set -eou pipefail
 
+echo $PACKAGE
+echo $CURRENT_VERISON
+
 # Check that there's a diff from the updater script. See https://stackoverflow.com/questions/3878624/how-do-i-programmatically-determine-if-there-are-uncommitted-changes.
 if git diff-index --quiet HEAD --; then
     echo "No diff after running updater."
