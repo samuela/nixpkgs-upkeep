0671f030a08c412c9228683d0be0fc9483770285
Samuel Ainsworth <skainsworth@gmail.com>
2022-03-02 00:19:59 +0000

    remove MKL from the overlay

--- overlay.nix
+++ overlay.nix
@@ -1,6 +1,10 @@
 final: prev: {
+  # There does exist cudatoolkit_11_6, but it does not have a corresponding
+  # cudnn version at the moment. See https://github.com/NixOS/nixpkgs/pull/158114.
   cudatoolkit = prev.cudatoolkit_11_5;
   cudnn = prev.cudnn_8_3_cudatoolkit_11_5;
-  blas = prev.blas.override { blasProvider = final.mkl; };
-  lapack = prev.lapack.override { lapackProvider = final.mkl; };
+  # See https://discourse.nixos.org/t/getting-different-results-for-the-same-build-on-two-equally-configured-machines/17921
+  # as to why I don't use MKL by default anymore.
+  # blas = prev.blas.override { blasProvider = final.mkl; };
+  # lapack = prev.lapack.override { lapackProvider = final.mkl; };
 }
