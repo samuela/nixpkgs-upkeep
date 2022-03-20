8c68962a2f634172fb586bc3e5ff3796a42e8e98
Samuel Ainsworth <skainsworth@gmail.com>
2022-03-20 06:15:45 +0000

    update-jax.py: sha256 -> hash, use SRI format

--- update-jax.py
+++ update-jax.py
@@ -25,12 +25,16 @@ print(f"Found release {tag_name}")
 assert tag_name.startswith("jax-v")
 major, minor, patch = tag_name[5:].split(".")
 
-sha256 = subprocess.check_output(["nix-prefetch-url", "--unpack", tarball_url],
-                                 text=True).strip()
+# nix-prefetch-url doesn't output SRI hashes for some reason. See https://discourse.nixos.org/t/why-does-nix-prefetch-url-not-return-hashes-in-sri-format/18271.
+sha256 = subprocess.check_output(
+    ["nix-prefetch-url", "--type", "sha256", "--unpack", tarball_url],
+    text=True).strip()
+srihash = subprocess.check_output(
+    ["nix", "hash", "to-sri", "--type", "sha256", sha256], text=True).strip()
 
 nix_path = os.path.join(os.path.dirname(__file__), "default.nix")
 nix0 = open(nix_path, "r").read()
 nix1 = re.sub("version = \".*\";", f"version = \"{major}.{minor}.{patch}\";",
               nix0)
-nix2 = re.sub("sha256 = \".*\";", f"sha256 = \"{sha256}\";", nix1)
+nix2 = re.sub(r"(sha256|hash) = \".*\";", f"hash = \"{srihash}\";", nix1)
 open(nix_path, "w").write(nix2)
